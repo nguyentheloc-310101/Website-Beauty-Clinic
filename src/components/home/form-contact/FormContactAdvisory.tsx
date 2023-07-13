cf8a70055d89980e6bf1943fe4675107f1e0a2bc
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-13 12:36:52 +0700

    changing API

--- src/components/home/form-contact/FormContactAdvisory.tsx
+++ src/components/home/form-contact/FormContactAdvisory.tsx
@@ -29,7 +29,7 @@ const FormContactSmall = () => {
     console.log(data);
     //api send
     setLoading(true);
-    await fetch('http://localhost:8080/api/contact', {
+    await fetch('http://staging.thammyaura.vn/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -41,7 +41,7 @@ const FormContactSmall = () => {
         service,
       }),
     });
-    await fetch('http://localhost:8080/api/send-message', {
+    await fetch('http://staging.thammyaura.vn/api/send-message', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
