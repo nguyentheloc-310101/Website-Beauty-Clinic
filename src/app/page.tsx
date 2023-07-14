d855eb12e89afbd61d968a37470a3d1f55c55107
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-14 15:16:10 +0700

    checkout staging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ import { Metadata } from 'next';
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>This page has a title 🤔</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+      </Head>
       <HomePage />
     </div>
   );
