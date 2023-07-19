5f0ba1aa42a96cd515b02b6fa31659df1d4c1919
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-19 18:38:16 +0700

    open comment video link

--- src/components/home/customer-said-video/CustomerSaidVideo.tsx
+++ src/components/home/customer-said-video/CustomerSaidVideo.tsx
@@ -37,8 +37,7 @@ const CustomerSaidVideo = () => {
           <section className="max-w-[980px] mx-auto relative w-full  z-50">
             <div className="pt-[56.25%] relative">
               <iframe
-                // src="https://www.youtube.com/embed/mfZ-KSP_iA4?&autoplay=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=mfZ-KSP_iA4"
-                src=""
+                src="https://www.youtube.com/embed/mfZ-KSP_iA4?&autoplay=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=mfZ-KSP_iA4"
                 className="absolute rounded-[40px] bg-black top-0 left-0 w-full h-full object-contain"
                 allowFullScreen
               />
@@ -48,15 +47,13 @@ const CustomerSaidVideo = () => {
           <section className="max-w-[360px] mx-auto relative w-full mt-[5px]  z-50">
             <div className="pt-[56.25%] relative">
               <iframe
-                // src="https://www.youtube.com/embed/mfZ-KSP_iA4?&autoplay=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=mfZ-KSP_iA4"
-                src=""
+                src="https://www.youtube.com/embed/mfZ-KSP_iA4?&autoplay=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=mfZ-KSP_iA4"
                 className="absolute rounded-[40px] bg-black top-0 left-0 w-full h-full object-contain"
                 allowFullScreen
               />
             </div>
           </section>
         )}
-
         <img
           src={
             'https://ucarecdn.com/ae813e5a-99e3-4c9d-9c2d-f8671a02b73c/-/preview/-/quality/lightest/-/format/webp/'
