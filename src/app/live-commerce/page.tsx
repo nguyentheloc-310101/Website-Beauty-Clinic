46c860141990ffa89bc59769407775cd360a9c14
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-20 11:25:47 +0700

    fix livestream

--- src/app/live-commerce/page.tsx
+++ src/app/live-commerce/page.tsx
@@ -1,18 +1,26 @@
 'use client';
-import ReelsOutStanding from "@/components/reels/ReelsOutStanding";
-import VideoReplay from "@/components/reels/VideoReplay";
+import ReelsOutStanding from '@/components/reels/ReelsOutStanding';
+import VideoReplay from '@/components/reels/VideoReplay';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function LiveCommerce() {
   return (
-    <div>
-      <Head>
-        <title>Live Commerce</title>
-        <meta property="og:title" content="Live Commerce" key="title" />
-      </Head>
-      <ReelsOutStanding/>
-      <div className="h-[10vh]"></div>
-      <VideoReplay/>
-    </div>
+    <>
+      <Script src="https://sf16-scmcdn-sg.ibytedtos.com/obj/static-sg/livesaas-client/pc/byteplus/js/index.1.1.2-beta.2.js" />
+      <div>
+        <Head>
+          <title>Live Commerce</title>
+          <meta
+            property="og:title"
+            content="Live Commerce"
+            key="title"
+          />
+        </Head>
+        <ReelsOutStanding />
+        <div className="h-[10vh]"></div>
+        <VideoReplay />
+      </div>
+    </>
   );
 }
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 // //Product
 
@@ -7,16 +7,16 @@ import { createClient } from "@supabase/supabase-js";
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodWtqeGZva29lYWNvYmJhamFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM5NDc3MjcsImV4cCI6MTk3OTUyMzcyN30.LndnChFvtsHpCcF9WZTCP67SsaYzfmDEDJQAmlWgnWc";
 // export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-//Staging
+// //Staging
 
-export const supabase = createClient(
-  "https://wmysxhtwbtmjxrrczjuj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndteXN4aHR3YnRtanhycmN6anVqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2OTE5OTAxNCwiZXhwIjoxOTg0Nzc1MDE0fQ.6epaFA-vn3pHGXqXCe55N8MMWIIEsjCoZZrJoJbGsJU"
-);
+// export const supabase = createClient(
+//   "https://wmysxhtwbtmjxrrczjuj.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndteXN4aHR3YnRtanhycmN6anVqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2OTE5OTAxNCwiZXhwIjoxOTg0Nzc1MDE0fQ.6epaFA-vn3pHGXqXCe55N8MMWIIEsjCoZZrJoJbGsJU"
+// );
 
-// //Aura id v2
+//Aura id v2
 
-// const supabaseUrl = "https://ctxvsrfwmbfxsodwuvxv.supabase.co";
-// const supabaseAnonKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0eHZzcmZ3bWJmeHNvZHd1dnh2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzQwMTc2OCwiZXhwIjoxOTgyOTc3NzY4fQ.ILlCsy7X1bBf5DQ2ccj4eHR3sK-cvStCnDEHLMtFOA0";
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = 'https://ctxvsrfwmbfxsodwuvxv.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0eHZzcmZ3bWJmeHNvZHd1dnh2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzQwMTc2OCwiZXhwIjoxOTgyOTc3NzY4fQ.ILlCsy7X1bBf5DQ2ccj4eHR3sK-cvStCnDEHLMtFOA0';
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
