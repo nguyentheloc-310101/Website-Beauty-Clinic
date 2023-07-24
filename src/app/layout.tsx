57f68712dc03b783fce1806e33bdc52abff76a99
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-24 09:50:13 +0700

    checkout amin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import FooterAura from '@/components/footer/FooterAura';
 import PanelContact from '@/components/panel-contact/PanelContact';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
