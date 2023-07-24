d40e42dc5f3186d96c74e7fee61c1932cbcd4f0d
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-07-24 12:19:56 +0700

    set up new reusable data service

---
+++ src/components/common/drop-down/DropDownGroup.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+import { Service } from '@/interfaces/service/service';
+
+interface DropDownGroupProp {
+  key: React.Key;
+  service_title: string;
+  service_list: Service[];
+}
+
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    type: 'group',
+    label: 'Group title',
+    children: [
+      {
+        key: '1-1',
+        label: '1st menu item',
+      },
+      {
+        key: '1-2',
+        label: '2nd menu item',
+      },
+    ],
+  },
+  {
+    key: '2',
+    type: 'group',
+    label: 'Group title',
+    children: [
+      {
+        key: '1-1',
+        label: '1st menu item',
+      },
+      {
+        key: '1-2',
+        label: '2nd menu item',
+      },
+    ],
+  },
+];
+
+const DropDownTitle = () => (
+  <Dropdown menu={{ items }}>
+    <a onClick={(e) => e.preventDefault()}>
+      <Space>
+        Cascading menu
+        <DownOutlined />
+      </Space>
+    </a>
+  </Dropdown>
+);
+
+export default DropDownTitle;
---
+++ src/data/header/serviceGroup.ts
@@ -0,0 +1,107 @@
+export const categoryData = [
+  {
+    category_name: 'Phẩu thuật thẩm mỹ',
+    group: true,
+    service_group: [
+      {
+        serviceGroupTitle: 'Mắt',
+        services: [
+          'Nhấn mí',
+          'Cắt mí MiniDeep 6D',
+          'Cắt Mí Dove Eyes',
+          'Treo cung nội bì',
+          'Cắt mí dưới',
+        ],
+      },
+      {
+        serviceGroupTitle: 'Mũi',
+        services: [
+          'Nội khoa: Nâng sống mũi',
+          'Nội khoa: Nâng đầu mũi',
+          'Ngoại khoa: Thu gọn cánh mũi',
+          'Ngoại khoa: Tạo hình dáng mũi',
+          'Ngoại khoa: Nâng mũi',
+        ],
+      },
+      {
+        serviceGroupTitle: 'Xử lý gồ',
+        services: [],
+      },
+    ],
+  },
+  {
+    category_name: 'Điều trị nám',
+    group: false,
+    services: ['Môi', 'Chân mày'],
+  },
+  {
+    category_name: 'Phun xăm',
+    group: true,
+    service_group: [
+      {
+        serviceGroupTitle: 'Môi',
+        services: ['Phun/ Cấy Nano', 'Sexy Lips'],
+      },
+      {
+        serviceGroupTitle: 'Chân mày',
+        services: ['Shading', 'Hairstroke/ Wings Hairstroke'],
+      },
+      {
+        serviceGroupTitle: 'Mí',
+        services: ['Mở tròng', 'Đá đuôi'],
+      },
+      {
+        serviceGroupTitle: 'Nhũ hoa',
+        services: [],
+      },
+      {
+        serviceGroupTitle: 'Xóa xăm/hút mực',
+        services: [],
+      },
+      {
+        serviceGroupTitle: 'Nốt ruồi phong thủy',
+        services: [],
+      },
+    ],
+  },
+  {
+    category_name: 'Tạo hình không xâm lấn',
+    group: false,
+    service: [
+      'HA Collagen',
+      'Botox',
+      'Căng chỉ',
+      'Lipozero Magic (Mỡ tự thân)',
+    ],
+  },
+  {
+    category_name: 'Nâng cơ trẻ hóa',
+    group: true,
+    service_group: [
+      {
+        serviceGroupTitle: 'Xóa nhăn',
+        services: ['Bioyoung', 'Oligio RF', 'HIFU PLUS 4.0'],
+      },
+      {
+        serviceGroupTitle: 'Trẻ hóa tầng sâu',
+        services: [
+          '  Diamond Lifting',
+          '  Golden Ultrasound',
+          '  Lippzero Magic',
+          '  Bro-shape Facial',
+        ],
+      },
+    ],
+  },
+  {
+    category_name: 'Dịch vụ khác',
+    group: false,
+    service: [
+      'Giảm Béo',
+      'Điều trị mụn',
+      'Detox da',
+      'Dương sinh',
+      'Tăm trắng',
+    ],
+  },
+];
