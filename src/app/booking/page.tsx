ca2cd7b518e6fba69a1be00b9ff54df3a2ec29ab
Loc Shecom <locshecom@Locs-Mac-mini.local>
2023-08-01 19:19:56 +0700

    mobile popup

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -16,7 +16,7 @@ const BookingPage = () => {
   return (
     <div className="mt-[12px] lg:mt-[2px] flex flex-col px-[16px] lg:grid lg:grid-cols-2 lg:px-[130px] lg:py-[30px] lg:gap-[24px]">
       <div className="flex flex-col lg:flex-col lg:gap-[24px]">
-        <div>
+        <div className="date-picker-antd">
           <CalendarBooking setDateBooking={setDateBooking} />
         </div>
         <div className="">
