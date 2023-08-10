c260aee97bf6c56976329165312c1dfadeb9363c
Loc Shecom <locshecom@Locs-Mac-mini.local>
2023-08-10 12:34:48 +0700

    hot fix

--- src/components/home/actor-said/ActorSaid.tsx
+++ src/components/home/actor-said/ActorSaid.tsx
@@ -7,15 +7,15 @@ const ActorSaid = () => {
   const { generalData } = useGeneralHomeContext();
   const celebFeedback: ICelebFeedback = generalData?.data.celebFeedback;
   return (
-    <div className="grid grid-cols-2 lg:px-[130px] sm:mt-[80px] lg:mt-[100px] sm:h-fit">
-      <div className="col-span-1 h-fit relative">
+    <div className="grid grid-cols-2 lg:gap-[24px] lg:px-[130px] sm:mt-[80px] lg:mt-[100px] sm:h-fit">
+      <div className="col-span-1 h-fit relative lg:flex justify-center items-center">
         <img
           src={celebFeedback?.image + '/-/quality/smart/-/format/webp/'}
           alt="actor-img"
           className="md:w-[555px] md:h-[891px]"
           style={{ objectFit: 'cover' }}
         />
-        <div className="lg:absolute lg:bottom-[200px] w-full text-left h-[40px]">
+        <div className="lg:absolute flex flex-col items-center lg:bottom-[200px] w-full text-left h-[40px]">
           <div className="text-[#4f4f4f] mt-[5px] italic sm:font-[300] text-[9px] uppercase ms:text-[12px] md:text-[18px] lg:font-[100] lg:text-[27px]">
             {celebFeedback?.job}
           </div>
@@ -24,7 +24,7 @@ const ActorSaid = () => {
           </div>
         </div>
       </div>
-      <div className="col-span-1 lg:mt-[66px] flex flex-col items-end">
+      <div className="col-span-1 lg:mt-[66px] flex flex-col items-center">
         <div className="lg:w-[600px] text-right uppercase lg:text-[56px] lg:font-[900] lg:leading-[64px] text-[#BC2449]">
           <div className="lg:mr-[60px]"> Aura là</div>
           <br />
@@ -38,7 +38,7 @@ const ActorSaid = () => {
           <div className="text-[10px] font-[400] w-[150px] text-[#414141] md:w-[350px] md:text-[18px] lg:w-[508px] lg:h-auto lg:text-[22px] lg:font-[300]">
             {celebFeedback?.content}
           </div>
-          <div className=" sm:font-[500] text-transparent bg-clip-text bg-gradient-to-r from-[#BF264B] to-[#DF4B6E] mr-[20px] text-[8px] md:text-[18px] lg:text-[26px] font-[400] lg:leading-[34px] lg:tracking-[1px]">
+          <div className=" sm:font-[500] lg:py-[20px] text-transparent bg-clip-text bg-gradient-to-r from-[#BF264B] to-[#DF4B6E] mr-[20px] text-[8px] md:text-[18px] lg:text-[26px] font-[400] lg:leading-[34px] lg:tracking-[1px]">
             {celebFeedback?.tagline}
           </div>
         </div>
