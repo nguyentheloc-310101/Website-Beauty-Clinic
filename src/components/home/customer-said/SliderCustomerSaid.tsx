0432848a8261bde7fa04117810bd3eab1b66cf43
Loc Shecom <locshecom@Locs-Mac-mini.local>
2023-08-10 12:30:09 +0700

    hot fix

--- src/components/home/customer-said/SliderCustomerSaid.tsx
+++ src/components/home/customer-said/SliderCustomerSaid.tsx
@@ -11,9 +11,13 @@ const SliderCustomerSaid = () => {
   return (
     <div className="background-customer-said">
       <div className="hidden lg:flex lg:items-center lg:justify-center">
-        <div className="mt-[80px] text-[#fff]  font-[800] text-[31px] w-[220px] h-[62px]">
-          Khách hàng
-          <div>nói gì về Aura</div>
+        <div className="mt-[80px] text-[#fff]  font-[800] text-[31px] ">
+          <div className="lg:text-[56px] font-[900] lg:leading-[64px] uppercase ">
+            Khách hàng
+          </div>
+          <div className="font-service-card lg:text-[120px] font-[400] lg:leading-[148px]">
+            nói gì về Aura?
+          </div>
         </div>
       </div>
       <div className="lg:hidden flex items-center justify-center z-10">
